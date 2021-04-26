5bc4c7d7d489a93d2ad100713a8f25f52cc7715f
Emilio Velasquez 18352 <68131403+vel18352@users.noreply.github.com>
2021-04-25 20:17:32 -0600

    Efectos de sonido
    Se creo un archivo con una funcion la cual se encarga de reproducir los efectos de sonido e intro del juego

---
+++ tiva_code/Sounds/Sounds.ino
@@ -0,0 +1,85 @@
+const int buzzer = PD_7;
+const float songSpeed = 1.0;
+
+#define C4 262
+#define C4s 277
+#define D4 294
+#define D4s 311
+#define E4 330
+#define F4 349
+#define F4s 370
+#define G4 392
+#define G4s 415
+#define A4 440
+#define A4s 466
+#define B4 494
+#define C5 523
+#define C5s 554
+#define D5 587
+#define D5s 622
+#define E5 659
+#define F5 698
+#define F5s 740
+#define G5 784
+#define G5s 831
+#define A5 880
+#define A5s 932
+#define B5 988
+#define C6 1109
+
+int intro_notes[] = {
+    B4, 0, B5, 0, F5s, 0, D5s, 0, B5, F5s, 0, D5s, 0,
+    C5, 0, C6, 0, G5, 0, E5, 0, C6, G5, 0, E5, 0,
+    B4, 0, B5, 0, F5s, 0, D5s, 0, B5, F5s, 0, D5s, 0,
+    D5s, E5, F5, 0, F5, F5s, G5, 0, G5, G5s, A5, 0, B5, 0,
+};
+int intro_durations[] = {
+    70, 70, 70, 70, 70, 70, 70, 70, 70, 70, 70, 210, 70,
+    70, 70, 70, 70, 70, 70, 70, 70, 70, 70, 140, 210, 70,
+    70, 70, 70, 70, 70, 70, 70, 70, 70, 70, 70, 210, 70,
+    70, 70, 70, 70, 70, 70, 70, 70, 70, 70, 70, 70, 140, 140,
+    };
+
+    
+int paco_notes[] = { E4, A4s, D5s, F5s, A5, 0, B5, G5s, F5, C5s, G4s};
+int paco_durations[] = {18, 18, 18, 18, 18, 53, 18, 18, 18, 18, 18};
+
+
+int death_notes[] = {
+    E5, F5, F5s, D5s, E5, D5, D5s, C5, C5s, B4, C5, 0,
+    G4s, 0, F4s, 0, F4s
+}; 
+int death_durations[] = {
+    80, 80, 80, 80, 80, 80, 80, 80, 80, 80, 80, 125, 
+    80, 125, 125, 125, 125
+    };
+    
+void setup()
+{
+
+}
+
+void play_sound (int notes[], int durations[], int sizenotes){
+  const int totalNotes = sizenotes / sizeof(int);
+  for (int i = 0; i < totalNotes; i++)
+  {
+    const int currentNote = notes[i];
+    float wait = durations[i] / songSpeed;
+    if (currentNote != 0)
+    {
+      tone(buzzer, notes[i], wait); 
+    }
+    else
+    {
+      noTone(buzzer);
+    }
+    delay(wait);
+  }
+}
+
+void loop()
+{
+  play_sound(intro_notes, intro_durations, sizeof(intro_notes));
+  play_sound(paco_notes, paco_durations, sizeof(paco_notes));
+  play_sound(death_notes, death_durations, sizeof(death_notes));
+}
